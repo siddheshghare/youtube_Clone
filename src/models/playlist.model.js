95e85a72f65ece7a99a3e007ab7a0ca0aaa9167b
siddheshghare <siddheshghare6112@gmail.com>
2024-12-30 13:06:14 +0530

    create comment,like,playlist,tweet model

---
+++ src/models/playlist.model.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose"
+
+const playlistSchema = new Schema({
+    video: {
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        
+    },
+    owner:{
+         type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true });
+
+
+
+export const Playlist = mongoose.model("Playlist", playlistSchema)
